d35cf644fd2f87d3c449bf18dc31049906c020a1
Jolien Franke <jolienfranke@gmail.com>
2023-10-17 09:27:49 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/se12/jolien/FinishedProjectRegister/FinishedProjectRegisterApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.jolien.FinishedProjectRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinishedProjectRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinishedProjectRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se12/jolien/FinishedProjectRegister/FinishedProjectRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.jolien.FinishedProjectRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinishedProjectRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
