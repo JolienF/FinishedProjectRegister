680b885ba99cb934ea5381def719e3d00af2f27c
Jolien Franke <jolienfranke@gmail.com>
2023-10-18 09:28:10 +0200

    Form to add projects, fixed dependancy bug

--- src/main/java/nl/miwnn/se12/jolien/FinishedProjectRegister/controller/FinishedProjectController.java
+++ src/main/java/nl/miwnn/se12/jolien/FinishedProjectRegister/controller/FinishedProjectController.java
@@ -1,9 +1,13 @@
 package nl.miwnn.se12.jolien.FinishedProjectRegister.controller;
 
+import nl.miwnn.se12.jolien.FinishedProjectRegister.model.FinishedProject;
 import nl.miwnn.se12.jolien.FinishedProjectRegister.repository.FinishedProjectRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Jolien Franke <j.franke@st.hanze.nl>
@@ -25,5 +29,24 @@ public class FinishedProjectController {
         model.addAttribute("allFinishedProjects", finishedProjectRepository.findAll());
 
         return "finishedProjectOverview";
+    }
+
+    @GetMapping("/finishedProject/new")
+    private String showFinishedProjectFrom(Model model) {
+        model.addAttribute("finishedProject", new FinishedProject());
+
+        return "finishedProjectFrom";
+    }
+
+    @PostMapping("/finishedProject/new")
+    private String saveOrUpdateFinishedProject(
+            @ModelAttribute("finishedProject") FinishedProject finishedProjectToBeSaved, BindingResult result) {
+        if ((!result.hasErrors())) {
+            finishedProjectRepository.save(finishedProjectToBeSaved);
+        } else {
+            System.err.println(result.getAllErrors());
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/nl/miwnn/se12/jolien/FinishedProjectRegister/model/FinishedProject.java
+++ src/main/java/nl/miwnn/se12/jolien/FinishedProjectRegister/model/FinishedProject.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 /**
  * @author Jolien Franke <j.franke@st.hanze.nl>
  * Properties of a finished project that enters our register
@@ -17,11 +18,29 @@ public class FinishedProject {
 
     private String projectName;
 
+    private String finishDate;
+
     public Long getProjectId() {
         return projectId;
     }
 
     public String getProjectName() {
         return projectName;
+    }
+
+    public String getFinishDate() {
+        return finishDate;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public void setFinishDate(String finishDate) {
+        this.finishDate = finishDate;
     }
 }
